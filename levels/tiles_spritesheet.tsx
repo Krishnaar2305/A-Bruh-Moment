0cc0e9ad65734af4acbc2dfc666c0fb2528f9bd6
Krishnaar2305 <krishnaarora2305@gmail.com>
2024-05-18 00:50:20 +0530

    empytied

--- levels/tiles_spritesheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tiles_spritesheet" tilewidth="70" tileheight="70" spacing="10" tilecount="121" columns="11">
- <image source="../images/sprites/Base pack/Tiles/tiles_spritesheet.png" width="914" height="936"/>
-</tileset>
