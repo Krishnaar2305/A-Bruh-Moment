57f9974a1a7b36232ef7ca9198347507b26cfeb3
Krishnaar2305 <krishnaarora2305@gmail.com>
2024-05-18 00:51:04 +0530

    entire game

---
+++ levels/enemies.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="enemies" tilewidth="66" tileheight="146" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="51" height="51" source="../images/sprites/Base pack/Enemies/blockerMad.png"/>
+ </tile>
+ <tile id="1">
+  <image width="66" height="42" source="../images/sprites/Base pack/Enemies/fishSwim1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="62" height="43" source="../images/sprites/Base pack/Enemies/fishSwim2.png"/>
+ </tile>
+ <tile id="3">
+  <image width="48" height="146" source="../images/sprites/Base pack/Enemies/pokerMad.png"/>
+ </tile>
+ <tile id="4">
+  <image width="50" height="28" source="../images/sprites/Base pack/Enemies/slimeWalk1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="51" height="26" source="../images/sprites/Base pack/Enemies/slimeWalk2.png"/>
+ </tile>
+</tileset>
---
+++ levels/ground.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="ab" tilewidth="70" tileheight="70" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="70" height="70" source="../images/sprites/Base pack/Tiles/bridge.png"/>
+ </tile>
+ <tile id="1">
+  <image width="70" height="70" source="../images/sprites/Base pack/Tiles/grass.png"/>
+ </tile>
+ <tile id="2">
+  <image width="70" height="70" source="../images/sprites/Base pack/Tiles/grassCenter.png"/>
+ </tile>
+ <tile id="3">
+  <image width="70" height="70" source="../images/sprites/Base pack/Items/flagRed.png"/>
+ </tile>
+ <tile id="4">
+  <image width="70" height="70" source="../images/sprites/Base pack/Tiles/liquidLava.png"/>
+ </tile>
+ <tile id="5">
+  <image width="70" height="70" source="../images/sprites/Base pack/Tiles/liquidLavaTop.png"/>
+ </tile>
+</tileset>
